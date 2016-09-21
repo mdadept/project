77b7eaa8cbaf3b4b380cc33c0bfb38330420a7cf
Alex Alex <mdadept@gmail.com>
2016-09-21 13:47:31 +0300

    first commit

---
+++ storage/smarty/cache/387530bcd486c516c9990061265a81c3b86ac2c3.smarty.tpl.php
@@ -0,0 +1,24 @@
+<?php
+/* Smarty version 3.1.30, created on 2016-09-21 08:25:47
+  from "C:\OS\latrell\resources\views\smarty.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_57e2440b7625a8_50415215',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '719f272884ffdf18c17d3b6f7eec55c833719d7f' => 
+    array (
+      0 => 'C:\\OS\\latrell\\resources\\views\\smarty.tpl',
+      1 => 1474441193,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 120,
+),true)) {
+function content_57e2440b7625a8_50415215 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<h1>Enter test string smarty</h1><?php }
+}
---
+++ storage/smarty/compile/719f272884ffdf18c17d3b6f7eec55c833719d7f_0.file.smarty.tpl.cache.php
@@ -0,0 +1,28 @@
+<?php
+/* Smarty version 3.1.30, created on 2016-09-21 07:00:59
+  from "C:\OS\latrell\resources\views\smarty.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_57e2302b4a7074_19105045',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '719f272884ffdf18c17d3b6f7eec55c833719d7f' => 
+    array (
+      0 => 'C:\\OS\\latrell\\resources\\views\\smarty.tpl',
+      1 => 1474441193,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_57e2302b4a7074_19105045 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '2963957e2302b33fa18_57040799';
+?>
+<h1><?php echo $_smarty_tpl->tpl_vars['test']->value;?>
+</h1><?php }
+}
